c87c41f848954ce9174e1b82cf03546bf4566eaf
DeveloperPyy <shashankldharpure@gmail.com>
2025-05-26 00:51:20 +0530

    Updated ROS 2 topic names to use bot2 namespace

--- ydlidar_ros2_driver/launch/ydlidar_launch.py
+++ ydlidar_ros2_driver/launch/ydlidar_launch.py
@@ -46,6 +46,7 @@ def generate_launch_description():
     tf2_node = Node(package='tf2_ros',
                     executable='static_transform_publisher',
                     name='static_tf_pub_laser',
+                    namespace='bot2',
                     arguments=['0', '0', '0','0', '0', '0', '1','bot2/base_link','bot2/laser_frame'],
                     )
 
